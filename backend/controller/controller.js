43b39693a8a7f4f4708e3e2aea70e475acb6aad3
Gaurab Shrestha <gaurab.stha.5@gmail.com>
2022-11-17 11:37:08 +0545

    Added controller for displaying data

---
+++ backend/controller/controller.js
@@ -0,0 +1,49 @@
+const mysqldb = require("../database/mysqldb");
+const { db } = require("../database/mongodb");
+const User = require("../model/UserModel");
+const firebaseDB = require("../database/firebasedb");
+
+//controller for getting data from mysql
+const getDataFromMySql = async (req, res) => {
+  mysqldb.query("SELECT * from user", (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(result);
+    }
+  });
+};
+
+//controller for getting data from mongodb
+const getDataFromMongoDB = async (req, res) => {
+  try {
+    User.find({}, (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//controller for getting data from firebase
+const getDataFromFireBase = async (req, res) => {
+  try {
+    const data = firebaseDB.collection("Users");
+    const snapshot = await data.get();
+    const allData = snapshot.docs.map((doc) => {
+      return doc.data();
+    });
+    return res.json(allData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getDataFromMySql,
+  getDataFromMongoDB,
+  getDataFromFireBase,
+};
