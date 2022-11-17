7ffa9882339c3606e6b9b338c4def658abc05719
Gaurab Shrestha <gaurab.stha.5@gmail.com>
2022-11-17 12:08:09 +0545

    Connection for MySQL Database

--- backend/database/mysqldb.js
+++ backend/database/mysqldb.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin123",
-  database: "portpro_test",
+const db = mysql.createPool({
+  host: process.env.LOCAL_HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.database,
 });
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
+
+if (db) {
   console.log("MySql Database is Connected");
-});
+} else {
+  console.log(error);
+}
 
 module.exports = db;
